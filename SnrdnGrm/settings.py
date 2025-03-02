f274dde3db2242bbe8cc336d78d25235af0bf335
sude-tunc <zeynepsude.tunc@ogr.gsu.edu.tr>
2025-03-02 16:10:27 +0300

    şifre unuutmm eklendi ama tm çalimio ve düzenlenmedi sayaflr

--- SnrdnGrm/settings.py
+++ SnrdnGrm/settings.py
@@ -138,3 +138,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'senin-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'tvup powx ayde anwp'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import register, profile
-from .forms import UserLoginForm  # Login formunu içe aktarıyoruz
+from .forms import UserLoginForm  # login formunu aktarioz
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html", authentication_form=UserLoginForm), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("profile/", profile, name="profile"),
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"), name="password_reset"), #sıfırlama bağlantısı için
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),#mail gitti bilgilendirme sayfası
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),#yeni şifre belirleme sayfası
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="password_reset_complete"),#şifre sısfırlama mesajı
+    
 ]
