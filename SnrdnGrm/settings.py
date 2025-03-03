87fe525f9aa0bfffbc49ed8a0562f154b9a0afea
zeynep sude tunc <zeynepsude.tunc@ogr.gsu.edu.tr>
2025-03-03 10:59:39 +0300

    Update settings.py bisi yaptim

--- SnrdnGrm/settings.py
+++ SnrdnGrm/settings.py
@@ -143,6 +143,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'senin-email@gmail.com'
+EMAIL_HOST_USER = 'sude11tunc@gmail.com'
 EMAIL_HOST_PASSWORD = 'tvup powx ayde anwp'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
