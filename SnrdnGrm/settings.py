59f245341df512d72bb40c79ff902e548e26be8d
sude-tunc <zeynepsude.tunc@ogr.gsu.edu.tr>
2025-03-07 13:00:49 +0300

    mail ayarı

--- SnrdnGrm/settings.py
+++ SnrdnGrm/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 #güvenli gönnderim için 587 
