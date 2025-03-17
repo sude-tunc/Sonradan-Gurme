20334bc54cb0461b571f055c8f6dba857a71759e
sude-tunc <zeynepsude.tunc@ogr.gsu.edu.tr>
2025-03-17 18:35:48 +0300

    giriş,welcome ve profil syfaları tasarım güncelledi

--- SnrdnGrm/settings.py
+++ SnrdnGrm/settings.py
@@ -145,6 +145,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = '/welcome/' #welcome page i buraya bğaldık
 LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
